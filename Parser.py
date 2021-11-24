5c2c7d21bd8bb9daef366d370774cc2ea3ae67f4
SergeyFeris <52924422+SergeyFeris@users.noreply.github.com>
2021-11-24 20:40:51 +0400

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -8,21 +8,22 @@ print('Введите номер дропа')
 drop = str(input())
 
 
-with open('adresses.txt','r', encoding="UTF-8" ) as readFile:
+with open('Bep-20.txt','r', encoding="UTF-8" ) as readFile:
     bep20 = readFile.readlines()
 if servise == 1:
     link = 'https://redkite-api.polkafoundry.com/user/winner-search/'
 if servise == 2:
     link = 'https://hub.gamefi.org/api/v1/user/winner-search/'
-
+i =0
 for adress in bep20:
     try:
+        i=i+1
         response = requests.get(link+drop+'?page=1&limit=10&search='+adress)
         info = json.loads(response.text)
         if info['data']['total'] == '1':
             print(adress)
         else:
-            print('не попал')
+            print(str(i) + ' не попал')
     except json.decoder.JSONDecodeError:
         print(response.text)
-        print(response) 
+        print(response)
